6ab5b15adc6247e1887e4cf85603bc31b78732bd
Matin Huseynzade <matinhuseynzade@gmail.com>
2025-02-06 10:25:38 +0300

    saa

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-// navbar.js
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("../components/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
-});
